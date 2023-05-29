d295fe326818e3399f22b159237d3b00294065b4
kevinalg8 <kalondono35@soy.sena.edu.co>
2023-05-29 00:34:32 -0500

    fixRoutes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,17 +1,18 @@
 import { Router } from "express";
-import { getUsers, getUser, createUsers, updateUsers, deleteUsers } from "../controllers/users.controller.js";
+//import { getUsers, getUser, createUsers, updateUsers, deleteUsers } from "../controllers/users.controller.js";
+import * as controllers from "../controllers/users.controller.js"
 
 const router = Router();
 
-  router.get("/users", getUsers) 
+  router.get("/users", controllers.getUsers) 
 
-  router.get("/users/:COD_USUARIO", getUser) 
+  router.get("/users/:COD_USUARIO", controllers.getUser) 
 
-  router.post("/users", createUsers) 
+  router.post("/users", controllers.createUsers) 
   
-  router.patch("/users/:COD_USUARIO", updateUsers)
+  router.patch("/users/:COD_USUARIO", controllers.createUsers)
   
-  router.delete("/users/:COD_USUARIO", deleteUsers)
+  router.delete("/users/:COD_USUARIO", controllers.deleteUsers)
   
 export default router;
 
