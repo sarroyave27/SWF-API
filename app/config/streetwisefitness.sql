91df70624ed3033b2819fe4faa37b58ce1d1440d
jhonatan032004 <jpalacio1137@gmail.com>
2023-06-23 10:01:10 -0500

    database

--- app/config/streetwisefitness.sql
+++ app/config/streetwisefitness.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 21-06-2023 a las 01:07:57
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 23-06-2023 a las 16:59:15
+-- Versión del servidor: 10.4.19-MariaDB
+-- Versión de PHP: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,72 +28,72 @@ DELIMITER $$
 -- Procedimientos
 --
 DROP PROCEDURE IF EXISTS `spConsultUser`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `spConsultUser` (IN `_CORREO` VARCHAR(50), IN `_CONTRASENA` LONGTEXT)   BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spConsultUser` (IN `_CORREO` VARCHAR(50), IN `_CONTRASENA` LONGTEXT)  BEGIN
 	SELECT CORREO,CONTRASENA,NOMBRES FROM usuario WHERE CORREO =_CORREO &&  CONTRASENA=_CONTRASENA;
 END$$
 
 DROP PROCEDURE IF EXISTS `spCreateAssis`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `spCreateAssis` (IN `_FECHA` DATE)   BEGIN 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spCreateAssis` (IN `_FECHA` DATE)  BEGIN 
 	INSERT INTO asistencia(FECHA) VALUES (_FECHA);
 END$$
 
 DROP PROCEDURE IF EXISTS `spCreatePlan`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `spCreatePlan` (IN `_NOMBRE` VARCHAR(100), IN `_DESCRIPCION` VARCHAR(100))   BEGIN 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spCreatePlan` (IN `_NOMBRE` VARCHAR(100), IN `_DESCRIPCION` VARCHAR(100))  BEGIN 
 INSERT INTO plan(NOMBRE,DESCRIPCION) VALUES (_NOMBRE,_DESCRIPCION);
 END$$
 
 DROP PROCEDURE IF EXISTS `spCreateRecipes`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `spCreateRecipes` (IN `_NOMBRE` VARCHAR(100), IN `_DESCRIPCION` VARCHAR(100), IN `_COD_USUARIO` INT(100))   BEGIN 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spCreateRecipes` (IN `_NOMBRE` VARCHAR(100), IN `_DESCRIPCION` LONGTEXT, IN `_COD_USUARIO` INT(100))  BEGIN 
 INSERT INTO recetas(NOMBRE,DESCRIPCION,COD_USUARIO) VALUES (_NOMBRE,_DESCRIPCION,_COD_USUARIO);
 END$$
 
 DROP PROCEDURE IF EXISTS `spCreateUsers`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `spCreateUsers` (IN `_NOMBRES` VARCHAR(50), IN `_APELLIDOS` VARCHAR(50), IN `_CORREO` VARCHAR(50), IN `_CELULAR` INT(10), IN `_FECHA_NACIMIENTO` DATE, IN `_CONTRASENA` LONGTEXT, IN `_COD_ROL` INT(10))   BEGIN 
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spCreateUsers` (IN `_NOMBRES` VARCHAR(50), IN `_APELLIDOS` VARCHAR(50), IN `_CORREO` VARCHAR(50), IN `_CELULAR` INT(10), IN `_FECHA_NACIMIENTO` DATE, IN `_CONTRASENA` LONGTEXT, IN `_COD_ROL` INT(10))  BEGIN 
 INSERT INTO usuario(NOMBRES,APELLIDOS,CORREO,CELULAR,FECHA_NACIMIENTO,CONTRASENA,COD_ROL) VALUES (_NOMBRES,_APELLIDOS,_CORREO,_CELULAR,_FECHA_NACIMIENTO,_CONTRASENA, _COD_ROL);
 END$$
 
 DROP PROCEDURE IF EXISTS `spDeletePlan`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `spDeletePlan` (IN `_COD_PLAN` INT(10))   BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spDeletePlan` (IN `_COD_PLAN` INT(10))  BEGIN
 	DELETE FROM plan WHERE COD_PLAN = _COD_PLAN; 
 END$$
 
 DROP PROCEDURE IF EXISTS `spDisableUser`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `spDisableUser` (IN `_COD_USUARIO` INT(100), IN `_ESTADO` BOOLEAN)   BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spDisableUser` (IN `_COD_USUARIO` INT(100), IN `_ESTADO` BOOLEAN)  BEGIN
 	UPDATE usuario SET ESTADO = _ESTADO WHERE COD_USUARIO= _COD_USUARIO;
 END$$
 
 DROP PROCEDURE IF EXISTS `spEditPlan`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `spEditPlan` (IN `_COD_PLAN` INT(10), IN `_DESCRIPCION` VARCHAR(100))   BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spEditPlan` (IN `_COD_PLAN` INT(10), IN `_DESCRIPCION` VARCHAR(100))  BEGIN
 	UPDATE plan SET DESCRIPCION = _DESCRIPCION WHERE COD_PLAN= _COD_PLAN;
 END$$
 
 DROP PROCEDURE IF EXISTS `spEditRecipes`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `spEditRecipes` (IN `_COD_RECETA` INT, IN `_NOMBRE` VARCHAR(100), IN `_DESCRIPCION` VARCHAR(100))   BEGIN
-	UPDATE recetas SET NOMBRE = _NOMBRE AND DESCRIPCION =_DESCRIPCION  WHERE COD_RECETA = _COD_RECETA;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spEditRecipes` (IN `_COD_RECETA` INT(100), IN `_NOMBRE` VARCHAR(100), IN `_DESCRIPCION` LONGTEXT)  BEGIN
+	UPDATE recetas SET NOMBRE = _NOMBRE, DESCRIPCION =_DESCRIPCION  WHERE COD_RECETA = _COD_RECETA;
 END$$
 
 DROP PROCEDURE IF EXISTS `spEditUser`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `spEditUser` (IN `_COD_USUARIO` INT(100), IN `_CONTRASENA` LONGTEXT, IN `_CELULAR` INT(20))   BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spEditUser` (IN `_COD_USUARIO` INT(100), IN `_CONTRASENA` LONGTEXT, IN `_CELULAR` INT(20))  BEGIN
 	UPDATE usuario SET CELULAR =_CELULAR AND CONTRASENA =_CONTRASENA  WHERE COD_USUARIO= _COD_USUARIO;
 END$$
 
 DROP PROCEDURE IF EXISTS `spFindAllPlans`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `spFindAllPlans` ()   BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spFindAllPlans` ()  BEGIN
 	SELECT COD_PLAN, NOMBRE, DESCRIPCION FROM plan;
 END$$
 
 DROP PROCEDURE IF EXISTS `spFindAllUsers`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `spFindAllUsers` ()   BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spFindAllUsers` ()  BEGIN
 	SELECT COD_USUARIO, NOMBRES, APELLIDOS, FECHA_NACIMIENTO,CORREO,COD_ROL,ESTADO,CELULAR,CONTRASENA FROM usuario;
 END$$
 
 DROP PROCEDURE IF EXISTS `spFindAPlan`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `spFindAPlan` (IN `_COD_PLAN` INT(10))   BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spFindAPlan` (IN `_COD_PLAN` INT(10))  BEGIN
 	SELECT NOMBRE,DESCRIPCION FROM plan WHERE COD_PLAN =_COD_PLAN;
 END$$
 
 DROP PROCEDURE IF EXISTS `spFindUser`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `spFindUser` (IN `_COD_USUARIO` INT(10))   BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spFindUser` (IN `_COD_USUARIO` INT(10))  BEGIN
 	SELECT COD_USUARIO,NOMBRES,APELLIDOS,CORREO,CELULAR,FECHA_NACIMIENTO FROM usuario WHERE COD_USUARIO =_COD_USUARIO;
 END$$
 
@@ -112,7 +112,7 @@ CREATE TABLE IF NOT EXISTS `asistencia` (
   `COD_USERPLAN` int(100) DEFAULT NULL,
   PRIMARY KEY (`COD_ASIS`),
   KEY `COD_USERPLAN` (`COD_USERPLAN`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -124,10 +124,18 @@ DROP TABLE IF EXISTS `plan`;
 CREATE TABLE IF NOT EXISTS `plan` (
   `COD_PLAN` int(10) NOT NULL AUTO_INCREMENT,
   `NOMBRE` varchar(100) NOT NULL,
-  `DESCRIPCION` varchar(100) NOT NULL,
+  `DESCRIPCION` longtext NOT NULL,
   PRIMARY KEY (`COD_PLAN`),
   UNIQUE KEY `NOMBRE` (`NOMBRE`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `plan`
+--
+
+INSERT INTO `plan` (`COD_PLAN`, `NOMBRE`, `DESCRIPCION`) VALUES
+(4, 'Recomposicion corporal', 'Entrenamiento basado en dietas para la recomposicion corporal y ejercicios aerobicos'),
+(5, 'Deficit calorico', 'Plan desarrollado para personas en sobre peso; basado en entrenamientos aerobicos y de perdida de calorias');
 
 -- --------------------------------------------------------
 
@@ -143,7 +151,7 @@ CREATE TABLE IF NOT EXISTS `planusuario` (
   PRIMARY KEY (`COD_USERPLAN`),
   KEY `COD_USUARIO` (`COD_USUARIO`),
   KEY `COD_PLAN` (`COD_PLAN`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -155,47 +163,26 @@ DROP TABLE IF EXISTS `recetas`;
 CREATE TABLE IF NOT EXISTS `recetas` (
   `COD_RECETA` int(100) NOT NULL AUTO_INCREMENT,
   `NOMBRE` varchar(100) NOT NULL,
-  `DESCRIPCION` varchar(100) NOT NULL,
+  `DESCRIPCION` longtext NOT NULL,
   `COD_USUARIO` int(100) DEFAULT NULL,
+  `estado` tinyint(1) NOT NULL,
   PRIMARY KEY (`COD_RECETA`),
   UNIQUE KEY `NOMBRE` (`NOMBRE`),
   KEY `COD_USUARIO` (`COD_USUARIO`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `recetas`
 --
 
-INSERT INTO `recetas` (`COD_RECETA`, `NOMBRE`, `DESCRIPCION`, `COD_USUARIO`) VALUES
-(3, 'Limonada de coco', 'Baja 80 Kilos ahora!', 1),
-(4, 'Baja Kilos 3000', 'Baja 20 kilos en media hora', 2),
-(5, 'Banana Coco', 'Baja 80kilos', 2),
-(12, 'Banano Monstruo', 'Baja Grasita', 2),
-(14, 'Banano Exotico', 'Que se lo que dios quiera', 1);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `recetas_usuarios`
---
-
-DROP TABLE IF EXISTS `recetas_usuarios`;
-CREATE TABLE IF NOT EXISTS `recetas_usuarios` (
-  `COD_RECETA_USUARIO` int(100) NOT NULL AUTO_INCREMENT,
-  `COD_RECETA` int(100) DEFAULT NULL,
-  `COD_USUARIO` int(100) DEFAULT NULL,
-  PRIMARY KEY (`COD_RECETA_USUARIO`),
-  KEY `COD_RECETA` (`COD_RECETA`),
-  KEY `COD_USUARIO` (`COD_USUARIO`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
---
--- Volcado de datos para la tabla `recetas_usuarios`
---
-
-INSERT INTO `recetas_usuarios` (`COD_RECETA_USUARIO`, `COD_RECETA`, `COD_USUARIO`) VALUES
-(1, NULL, 1),
-(2, NULL, 1);
+INSERT INTO `recetas` (`COD_RECETA`, `NOMBRE`, `DESCRIPCION`, `COD_USUARIO`, `estado`) VALUES
+(3, 'Limonada de coco', 'Baja 80 Kilos ahora!', 1, 0),
+(4, 'Baja Kilos 3000', 'Baja 20 kilos en media hora', 2, 0),
+(5, 'Banana Coco', 'Baja 80kilos', 2, 0),
+(12, 'Banano Monstruo', 'Baja Grasita', 2, 0),
+(14, 'Banano Exotico', 'Que se lo que dios quiera', 1, 0),
+(15, '200g carbohidratos', 'arroz y pasta', 1, 0),
+(17, 'arroz', 'carbohidratos', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -208,7 +195,7 @@ CREATE TABLE IF NOT EXISTS `rol` (
   `COD_ROL` int(10) NOT NULL,
   `ROL` varchar(100) NOT NULL,
   PRIMARY KEY (`COD_ROL`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `rol`
@@ -240,7 +227,7 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   UNIQUE KEY `CORREO` (`CORREO`),
   UNIQUE KEY `CELULAR` (`CELULAR`),
   KEY `COD_ROL` (`COD_ROL`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `usuario`
@@ -281,13 +268,6 @@ ALTER TABLE `planusuario`
 --
 ALTER TABLE `recetas`
   ADD CONSTRAINT `recetas_ibfk_1` FOREIGN KEY (`COD_USUARIO`) REFERENCES `usuario` (`COD_USUARIO`);
-
---
--- Filtros para la tabla `recetas_usuarios`
---
-ALTER TABLE `recetas_usuarios`
-  ADD CONSTRAINT `recetas_usuarios_ibfk_1` FOREIGN KEY (`COD_RECETA`) REFERENCES `recetas` (`COD_RECETA`),
-  ADD CONSTRAINT `recetas_usuarios_ibfk_2` FOREIGN KEY (`COD_USUARIO`) REFERENCES `usuario` (`COD_USUARIO`);
 
 --
 -- Filtros para la tabla `usuario`
